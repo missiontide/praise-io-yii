61faf11b61abf88655f6b5b4edbb280fcad3f7e6
missiontide <patcoronel@missiontide.com>
2022-06-20 07:25:13 -0400

    docker files

--- config/db.php
+++ config/db.php
@@ -2,9 +2,9 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => env('DB_DSN'),
+    'dsn' => 'mysql:host=db;dbname=praise-io',
     'username' => 'root',
-    'password' => '',
+    'password' => 'root',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
